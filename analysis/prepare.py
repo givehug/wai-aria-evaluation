6f84276e19f299ebbe56775a881ede540feae880
Volodymyr Boretskyi <vboretskyi@gmail.com>
2021-07-21 23:58:00 +0200

    Refactor data preparation script

--- analysis/prepare.py
+++ analysis/prepare.py
@@ -3,36 +3,27 @@ from pandas.core.indexes.base import Index
 import glob
 
 
-def merge_arc_files():
-    # TODO: delete this redundant dir
-    arc_files = glob.glob("../testing/arc/byLibrary/*")
+def merge_files(in_path, out_path):
+    files = glob.glob(in_path)
     total_df = pd.DataFrame()
-    for idx, file in enumerate(arc_files):
+    for idx, file in enumerate(files):
         with open(file) as file:
-            file_name = arc_files[idx]
+            file_name = files[idx]
             framework = file_name.split("/")[4].split("_")[0]
             library = file_name.split("/")[4].split("_")[1].split(".")[0]
             df = pd.read_csv(file, header=0, encoding='utf-8', index_col=1)
             del df['#']
-            del df['Test output']
+            del df['Output']
             total_df[framework + "/" + library] = df['ErrorCount']
-    total_df.to_csv("../testing/arc/errors.csv", sep=',', encoding='utf-8')
+    total_df.to_csv(out_path, sep=',', encoding='utf-8')
+
+
+def merge_arc_files():
+    merge_files("../testing/arc/byLibrary/*", "../testing/arc/errors.csv")
 
 
 def merge_axe_files():
-    # TODO: delete this redundant dir
-    arc_files = glob.glob("../testing/axe/byLibrary/*")
-    total_df = pd.DataFrame()
-    for idx, file in enumerate(arc_files):
-        with open(file) as file:
-            file_name = arc_files[idx]
-            framework = file_name.split("/")[4].split("_")[0]
-            library = file_name.split("/")[4].split("_")[1].split(".")[0]
-            df = pd.read_csv(file, header=0, encoding='utf-8', index_col=1)
-            del df['#']
-            del df['Description']
-            total_df[framework + "/" + library] = df['Errors']
-    total_df.to_csv("../testing/axe/errors.csv", sep=',', encoding='utf-8')
+    merge_files("../testing/axe/byLibrary/*", "../testing/axe/errors.csv")
 
 
 merge_arc_files()
