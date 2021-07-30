e7ab1ff33e4f40e191f054281b30c8efce591c43
Volodymyr Boretskyi <vboretskyi@gmail.com>
2021-07-30 22:36:11 +0200

    Add readme, port and engines

--- web/server/server.js
+++ web/server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const serveStatic = (...p) => express.static(path.join(__dirname, ...p));
 
